fbd487811aaf2db0e3a478c91836f9b111887e22
Konrad Fedorczyk <fedek6@gmail.com>
2022-08-09 21:49:43 +0200

    Finish plugin functionality

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,28 @@
 import path from 'path'
 import {visit} from 'unist-util-visit'
+import sizeOf from 'image-size'
 
-type a = {
-  aaa: number;
-  bbb: number;
+type Options = {
+  dir: string,
+  url: string,
 }
 
-export const test: a = {
-  aaa: 1,
-  bbb: 2
-}
+export function setImageSrc(options: Options) {
+  const { dir, url } = options;
 
-console.log(path);
-console.log(visit);
+  return (tree: any, file: any) => {
+    // Traverse nodes
+    visit(tree, 'element', (node) => {
+      if (node.tagName === 'img') {
+        const src = node.properties.src;
+        const absoluteUrl = path.join(url, src);
+        const serverPath = path.join(dir, src);
+        const dimensions = sizeOf(serverPath);
+
+        node.properties.width = dimensions.width;
+        node.properties.height = dimensions.height;
+        node.properties.src = absoluteUrl;
+      }
+    })
+  }
+}
